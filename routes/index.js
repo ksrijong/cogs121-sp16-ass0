481b8742e39cc6145ea4e281dd2a66e2870207d1
Kirk Srijongsirikul <ksrijong@ucsd.edu>
2016-04-06 23:21:27 -0700

    trying to push to heroku

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var models = require('../models');
 exports.view = function(req, res) {
     //var data = {data: []};
     //res.render("index", data);
+    
     models.Message
       .find()
       .sort('-date')
